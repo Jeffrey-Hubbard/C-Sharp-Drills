f31975925572b49ca6a42c7b536ac22de8e990e2
Jeffrey Hubbard <jeffrey.hubbard@gmail.com>
2018-05-29 11:10:21 -0700

    add p.35 drill ByFives

---
+++ ByFives/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByFives
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool tryAgain = true;
+
+            while (tryAgain)
+            {
+                Console.WriteLine("Please provide me an integer, and I will perform 3 math operations upon it and tell you the results:");
+                ProvidedNumber number = new ProvidedNumber();
+                try
+                {
+                    number.value = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("If I take your number and I multiply it by 5, I get " + number.MultiplyByFive());
+                    Console.WriteLine("If I take your number and raise it to the fifth power, I get " + number.ExponentFive());
+                    Console.WriteLine("If I take your number and add 5 to it, I get " + number.AddFive());
+                    tryAgain = false;
+
+                }
+                catch (System.FormatException ex)
+                {
+                    Console.WriteLine("Only provide me with integers, please! Error: " + ex.Message);
+                }
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ ByFives/ProvidedNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByFives
+{
+    class ProvidedNumber
+    {
+        public int value;
+
+        public int MultiplyByFive()
+        {
+            return value * 5;
+        }
+
+        public int ExponentFive()
+        {
+            return Convert.ToInt32( Math.Pow( Convert.ToDouble(value), Convert.ToDouble(5)));
+        }
+
+        public int AddFive()
+        {
+            return value + 5;
+        }
+    }
+}
