768f9799fe333218319e4a5a95c348289c750a68
Jeffrey Hubbard <jeffrey.hubbard@gmail.com>
2018-05-30 13:37:58 -0700

    Add complete p.48 drill NamedParameters

---
+++ NamedParameters/NumberProcessor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamedParameters
+{
+    class NumberProcessor
+    {
+        public void Process(int firstNumber, int secondNumber)
+        {
+            int answer = firstNumber * 11;
+            Console.WriteLine("The second number entered was " + secondNumber);
+        }
+    }
+}
---
+++ NamedParameters/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamedParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberProcessor numberProcessor = new NumberProcessor();
+
+            int firstNumber = 45;
+            int secondNumber = 99;
+
+            Console.WriteLine("First I will pass 45 and 99 to the method, which should result in the number 99 being displayed below:");
+
+            numberProcessor.Process(firstNumber, secondNumber);
+            Console.ReadLine();
+
+            Console.WriteLine("Now I will pass the same numbers, as named parameters, in the opposite order. This should also result in the number 99 being displayed below:");
+
+            numberProcessor.Process(secondNumber: secondNumber, firstNumber: firstNumber);
+            Console.ReadLine();
+
+
+        }
+    }
+}
