0e0204a308663c4592811e61c404b6b816cd1205
Jeffrey Hubbard <jeffrey.hubbard@gmail.com>
2018-05-31 12:40:12 -0700

    add generic parameter list, remove == operator overload (cannot compare different generic types)

--- Inheritance/Employee.cs
+++ Inheritance/Employee.cs
@@ -6,10 +6,13 @@ using System.Threading.Tasks;
 
 namespace Inheritance
 {
-    class Employee : Person, IQuittable
+    class Employee<T> : Person, IQuittable where T : IComparable<T>
     {
         public int Id { get; set; }
         public bool ActivelyEmployed = true;
+        public List<T> Things { get; set; }
+
+
         public void Quit()
         {
             if (ActivelyEmployed)
@@ -24,29 +27,36 @@ namespace Inheritance
 
 
         }
-        public static bool operator== (Employee firstEmployee, Employee secondEmployee)
-        {
-            if (firstEmployee.Id == secondEmployee.Id)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
 
-        public static bool operator !=(Employee firstEmployee, Employee secondEmployee)
-        {
-            if (firstEmployee.Id == secondEmployee.Id)
-            {
-                return false;
-            }
-            else
-            {
-                return true;
-            }
-        }
+        // The below code was used for a previous drill that specified checking the id property to 
+        // compare two employee objects, however, comparisons of objects with generic type
+        // parameters instantiated with different parameters cannot be done, so this code 
+        // no longer works. Instead, the main program just gets the id property from the employee and uses
+        // standard int == comparison.
+
+        //public static bool operator ==(Employee<T> firstEmployee, Employee<T> secondEmployee)
+        //{
+        //    if (firstEmployee.Id == secondEmployee.Id)
+        //    {
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }
+        //}
+
+        //public static bool operator !=(Employee<T> firstEmployee, Employee<T> secondEmployee)
+        //{
+        //    if (firstEmployee.Id == secondEmployee.Id)
+        //    {
+        //        return false;
+        //    }
+        //    else
+        //    {
+        //        return true;
+        //    }
+        //}
 
     }
 }
--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -10,15 +10,33 @@ namespace Inheritance
     {
         static void Main(string[] args)
         {
-            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student", Id = 90210 };
+            Employee<string> employee = new Employee<string>() { FirstName = "Sample", LastName = "Student", Id = 90210 };
             employee.SayName();
+            string[] traits = new string[] { "Ginger", "Freckles", "Graduate" };
+            employee.Things = new List<string>();
+            employee.Things.AddRange(traits);
+            Console.WriteLine(employee.FirstName + " has the following traits:");
+            foreach (string trait in employee.Things)
+            {
+                Console.WriteLine(trait);
+            }
             Console.ReadLine();
 
             Console.WriteLine("We have hired a new employee!");
-            IQuittable newEmployee = new Employee() { FirstName = "Roger", LastName = "Rabbit", Id = 92929 };
-            Console.WriteLine("Please welcome {0} {1}, employee number {2}!", ((Employee)newEmployee).FirstName, ((Employee)newEmployee).LastName, ((Employee)newEmployee).Id);
+            Employee<int> newEmployee = new Employee<int>() { FirstName = "Roger", LastName = "Rabbit", Id = 92929 }; // This employee used to be typed to IQuittable for drill purposes.
+            newEmployee.Things = new List<int>();
+            int[] testScores = { 98, 67, 99, 85, 85, 43, 100 };
+            newEmployee.Things.AddRange(testScores);
+            Console.WriteLine("Their test scores are:");
+            foreach (int testScore in newEmployee.Things)
+            {
+                Console.WriteLine(testScore);
+            }
+            Console.ReadLine();
 
-            Console.WriteLine("I have a feeling " + ((Employee)newEmployee).FirstName + " is going to quit! Let's watch what happens:");
+            Console.WriteLine("Please welcome {0} {1}, employee number {2}!", ((Employee<int>)newEmployee).FirstName, ((Employee<int>)newEmployee).LastName, ((Employee<int>)newEmployee).Id);
+
+            Console.WriteLine("I have a feeling " + ((Employee<int>)newEmployee).FirstName + " is going to quit! Let's watch what happens:");
             newEmployee.Quit();
             Console.ReadLine();
 
@@ -27,11 +45,11 @@ namespace Inheritance
             Console.ReadLine();
 
             Console.WriteLine("Wait, are they the same person?:");
-            Console.WriteLine(employee == newEmployee);
+            Console.WriteLine(employee.Id == newEmployee.Id);
             Console.ReadLine();
 
             Console.WriteLine("Just to confirm that each person matches themselves, does the second employee equal themselves?:");
-            Console.WriteLine(newEmployee == newEmployee);
+            Console.WriteLine(newEmployee.Id == newEmployee.Id);
             Console.ReadLine();
 
         }
