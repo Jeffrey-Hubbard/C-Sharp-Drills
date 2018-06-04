d03a10cf9f26d6fcafe8c9b74949ba8299486fb6
Jeffrey Hubbard <jeffrey.hubbard@gmail.com>
2018-06-04 10:35:46 -0700

    add p 71 drill on Structs

---
+++ Struct/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number myNumber = new Number(45.5m);
+
+            Console.WriteLine("The value of the property Amount for object myNumber is " + myNumber.Amount);
+            Console.ReadLine();
+        }
+    }
+}
